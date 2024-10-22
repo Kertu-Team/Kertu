2c9724ca90a23ae907beebf3ed9255ba1578e275
Devianceeee <121192683+Devianceeee@users.noreply.github.com>
2024-10-22 12:44:11 +0100

    list-page

--- Kertu.Solution/Kertu.InteractiveServer/Migrations/20241022095536_1233.Designer.cs
+++ Kertu.Solution/Kertu.InteractiveServer/Migrations/20241022095536_1233.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Kertu.InteractiveServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Kertu.InteractiveServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241022095536_1233")]
+    partial class _1233
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -288,14 +291,6 @@ namespace Kertu.InteractiveServer.Migrations
                     b.HasBaseType("Kertu.InteractiveServer.Data.KertuElement");
 
                     b.Property<string>("opis")
-                        .IsRequired()
-                        .HasColumnType("text");
-
-                    b.Property<string>("status")
-                        .IsRequired()
-                        .HasColumnType("text");
-
-                    b.Property<string>("taskname")
                         .IsRequired()
                         .HasColumnType("text");
 
---
+++ Kertu.Solution/Kertu.InteractiveServer/Migrations/20241022095936_deviance.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kertu.InteractiveServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class deviance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "status",
+                table: "KertuElements",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "taskname",
+                table: "KertuElements",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "KertuElements");
+
+            migrationBuilder.DropColumn(
+                name: "taskname",
+                table: "KertuElements");
+        }
+    }
+}
