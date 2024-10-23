9e73cfa47483b47cc2a450caac997ab125bf63e5
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-10-24 00:29:24 +0200

    refactor: add default value for string

--- Kertu.Solution/Kertu.InteractiveServer/Data/KertuElement.cs
+++ Kertu.Solution/Kertu.InteractiveServer/Data/KertuElement.cs
@@ -12,6 +12,6 @@ namespace Kertu.InteractiveServer.Data
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
     }
 }
