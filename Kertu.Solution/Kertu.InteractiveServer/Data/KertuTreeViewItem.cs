e5bcdf52bf22eab16fde64f9cf9de3ca25656ce7
hlepps <pomapiotrek@gmail.com>
2024-10-28 14:22:29 +0100

    feat: tree navigation panel

---
+++ Kertu.Solution/Kertu.InteractiveServer/Data/KertuTreeViewItem.cs
@@ -0,0 +1,21 @@
+﻿using Kertu.InteractiveServer.Data.KertuElements;
+
+namespace Kertu.InteractiveServer.Data
+{
+    /// <summary>
+    /// Class made for converting different kertu elements into common tree view representation
+    /// </summary>
+    public class KertuTreeViewItem
+    {
+        public KertuElement KertuElement { get; set; }
+        public List<KertuTreeViewItem> Children { get; set; } = new List<KertuTreeViewItem>();
+
+        public string Name { get { return KertuElement.Name; } set { KertuElement.Name = value; } }
+
+        public KertuTreeViewItem(KertuElement element)
+        {
+            KertuElement = element;
+        }
+
+    }
+}
