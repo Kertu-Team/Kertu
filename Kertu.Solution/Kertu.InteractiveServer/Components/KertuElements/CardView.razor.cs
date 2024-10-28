555753df8c1773702bb3f71c5e52ab0bd60a9490
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-10-28 22:05:00 +0100

    refactor(card): split template and code

---
+++ Kertu.Solution/Kertu.InteractiveServer/Components/KertuElements/CardView.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Kertu.InteractiveServer.Components.KertuElements
+{
+    public partial class CardView : ComponentBase
+    {
+        string _descriptionValue = string.Empty;
+        bool _busy;
+
+        async Task OnBusyClick()
+        {
+            _busy = true;
+            await Task.Delay(2000);
+            _busy = false;
+        }
+    }
+}
