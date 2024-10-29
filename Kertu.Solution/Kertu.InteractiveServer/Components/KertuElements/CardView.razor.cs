44cdadb1f4528496b002ba27d88425263d5d5437
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-10-29 13:16:15 +0100

    feat: card save

--- Kertu.Solution/Kertu.InteractiveServer/Components/KertuElements/CardView.razor.cs
+++ Kertu.Solution/Kertu.InteractiveServer/Components/KertuElements/CardView.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Kertu.InteractiveServer.Data;
 using Kertu.InteractiveServer.Data.KertuElements;
 using Microsoft.AspNetCore.Components;
+using Radzen;
 
 namespace Kertu.InteractiveServer.Components.KertuElements
 {
@@ -13,21 +14,33 @@ namespace Kertu.InteractiveServer.Components.KertuElements
         [Inject]
         ApplicationDbContext? DbContext { get; set; }
 
-        string? _title;
-        string? _description;
+        KertuCard? _card;
+        string _title = string.Empty;
+        string _description = string.Empty;
         bool _busy;
 
         protected override void OnInitialized()
         {
-            KertuCard? card = DbContext?.KertuElements.Find(IdValue) as KertuCard;
-            _title = card?.Name;
-            _description = card?.Description;
+            _card = DbContext?.KertuElements.Find(IdValue) as KertuCard;
+            if (_card is null)
+            {
+                return;
+            }
+            _title = _card.Name;
+            _description = _card.Description;
         }
 
         async Task OnBusyClick()
         {
+            if (_card is null)
+            {
+                return;
+            }
             _busy = true;
-            await Task.Delay(2000);
+            _card.Name = _title;
+            _card.Description = _description;
+            await Task.Run(() => DbContext?.SaveChanges());
+            NotificationService.Notify(new NotificationMessage { Severity = NotificationSeverity.Success, Summary = "Success", Duration = 4000 });
             _busy = false;
         }
     }
