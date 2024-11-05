d1aa5b4f2c552b984da835d0995277c5e0cda55a
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-11-05 13:52:31 +0100

    feat: profile menu without logout

---
+++ Kertu.Solution/Kertu.InteractiveServer/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Kertu.InteractiveServer.Components.Layout
+{
+    public partial class MainLayout : LayoutComponentBase, IDisposable
+    {
+        private string? _userEmail;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            _userEmail = authState.User.Identity?.Name;
+        }
+
+        [Parameter]
+        public string LightTheme { get; set; }
+
+        [Parameter]
+        public string DarkTheme { get; set; }
+
+        private string CurrentLightTheme =>
+            LightTheme
+            ?? ThemeService.Theme?.ToLowerInvariant() switch
+            {
+                "dark" => "default",
+                "material-dark" => "material",
+                "fluent-dark" => "fluent",
+                "material3-dark" => "material3",
+                "software-dark" => "software",
+                "humanistic-dark" => "humanistic",
+                "standard-dark" => "standard",
+                _ => ThemeService.Theme,
+            };
+
+        private string CurrentDarkTheme =>
+            DarkTheme
+            ?? ThemeService.Theme?.ToLowerInvariant() switch
+            {
+                "default" => "dark",
+                "material" => "material-dark",
+                "fluent" => "fluent-dark",
+                "material3" => "material3-dark",
+                "software" => "software-dark",
+                "humanistic" => "humanistic-dark",
+                "standard" => "standard-dark",
+                _ => ThemeService.Theme,
+            };
+
+        private bool _value;
+
+        protected override void OnInitialized()
+        {
+            ThemeService.ThemeChanged += OnThemeChanged;
+            _value = ThemeService.Theme != CurrentDarkTheme;
+        }
+
+        private void OnThemeChanged()
+        {
+            _value = ThemeService.Theme != CurrentDarkTheme;
+            InvokeAsync(StateHasChanged);
+        }
+
+        void Change()
+        {
+            ThemeService.SetTheme(!_value ? CurrentLightTheme : CurrentDarkTheme);
+        }
+
+        private string Icon => _value ? "dark_mode" : "light_mode";
+
+        public void Dispose()
+        {
+            ThemeService.ThemeChanged -= OnThemeChanged;
+        }
+    }
+}
