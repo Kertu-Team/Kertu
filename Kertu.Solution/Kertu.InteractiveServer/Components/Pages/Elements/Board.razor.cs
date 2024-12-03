92509b3ce645170805f54e2410a23471f52b2cd9
piotr-grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-12-03 11:56:54 +0100

    refactor: order

--- Kertu.Solution/Kertu.InteractiveServer/Components/Pages/Elements/Board.razor.cs
+++ Kertu.Solution/Kertu.InteractiveServer/Components/Pages/Elements/Board.razor.cs
@@ -6,17 +6,10 @@ namespace Kertu.InteractiveServer.Components.Pages.Elements
 {
     public partial class Board : ComponentBase
     {
-        [Parameter]
-        public required string Id { get; set; }
-
-        private int IdValue => int.Parse(Id);
-
-        // Filter items by zone value
         private readonly Func<MyTask, RadzenDropZone<MyTask>, bool> _itemSelector = (item, zone) =>
             item.Status == (Status)zone.Value && item.Status != Status.Deleted;
 
         private readonly Func<RadzenDropZoneItemEventArgs<MyTask>, bool> _canDrop = request =>
-            // Allow item drop only in the same zone, in "Deleted" zone or in the next/previous zone.
             request.FromZone == request.ToZone
             || (Status)request.ToZone.Value == Status.Deleted
             || Math.Abs((int)request.Item.Status - (int)request.ToZone.Value) == 1;
@@ -30,6 +23,11 @@ namespace Kertu.InteractiveServer.Components.Pages.Elements
             Completed,
             Deleted,
         }
+
+        [Parameter]
+        public required string Id { get; set; }
+
+        private int IdValue => int.Parse(Id);
 
         protected override void OnInitialized()
         {
@@ -46,7 +44,6 @@ namespace Kertu.InteractiveServer.Components.Pages.Elements
 
         private static void OnItemRender(RadzenDropZoneItemRenderEventArgs<MyTask> args)
         {
-            // Customize item appearance
             if (args.Item.Name == "Task2")
             {
                 args.Attributes["draggable"] = "false";
@@ -58,7 +55,6 @@ namespace Kertu.InteractiveServer.Components.Pages.Elements
                 args.Attributes["class"] = "rz-card rz-variant-filled rz-background-color-primary-light rz-color-on-primary-light";
             }
 
-            // Do not render item if deleted
             args.Visible = args.Item.Status != Status.Deleted;
         }
 
@@ -66,13 +62,11 @@ namespace Kertu.InteractiveServer.Components.Pages.Elements
         {
             if (args.FromZone != args.ToZone)
             {
-                // update item zone
                 args.Item.Status = (Status)args.ToZone.Value;
             }
 
             if (args.ToItem != null && args.ToItem != args.Item)
             {
-                // reorder items in same zone or place the item at specific index in new zone
                 _data.Remove(args.Item);
                 _data.Insert(_data.IndexOf(args.ToItem), args.Item);
             }
