8612e9accea013e76b1181ba242e29c9957af16d
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-12-17 13:33:41 +0100

    feat: board children display

--- Kertu.Solution/Kertu.InteractiveServer/Components/Pages/Elements/Board.razor.cs
+++ Kertu.Solution/Kertu.InteractiveServer/Components/Pages/Elements/Board.razor.cs
@@ -1,18 +1,32 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Kertu.InteractiveServer.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Radzen.Blazor;
 using Models = Kertu.InteractiveServer.Data.Models.Elements;
 
 namespace Kertu.InteractiveServer.Components.Pages.Elements
 {
     public partial class Board : ComponentBase
     {
-        private IList<Models.Card>? _data;
+        private readonly Func<Models.Element, RadzenDropZone<Models.Element>, bool> _itemSelector = (item, zone) =>
+            item.ParentID == (int?)zone.Value;
+
+        private string _title = string.Empty;
+        private IList<Models.Element>? _elements;
 
         [Parameter]
         public required string Id { get; set; }
 
         private int IdValue => int.Parse(Id);
 
-        protected override void OnInitialized() { }
+        [Inject]
+        private ApplicationDbContext DbContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            _title = DbContext.Boards.Find(IdValue).Name;
+            _elements = DbContext.Boards.Include(b => b.Children).Single(b => b.Id == IdValue).Children;
+        }
 
         private void CreateItem() { }
     }
